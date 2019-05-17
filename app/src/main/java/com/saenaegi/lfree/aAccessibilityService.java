59daf1a12e2d263600cd5f1040d29c6ac56bc7a0
tjtkd827 <tjtkd827@gmail.com>
2019-05-18 02:18:47 +0900

    살짝쿵 추가 m2
    졸리당

--- app/src/main/java/com/saenaegi/lfree/aAccessibilityService.java
+++ app/src/main/java/com/saenaegi/lfree/aAccessibilityService.java
@@ -15,6 +15,8 @@ public class aAccessibilityService extends AccessibilityService {
     private static final String TAG = "AccessibilityService";
     private TextToSpeech tts;              // TTS 변수 선언
 
+    AccessibilityNodeInfo source;
+    //int tempGestureId;
 
     @Override
     public void onCreate() {
@@ -42,6 +44,34 @@ public class aAccessibilityService extends AccessibilityService {
         });
     }
 
+    @Override
+    public boolean onGesture(int gestureId) {
+        //tempGestureId = gestureId;
+
+        switch(gestureId) {
+            //GESTURE_SWIPE_LEFT = 3
+            //GESTURE_SWIPE_RIGHT = 4
+            //GESTURE_SWIPE_UP = 1
+            //GESTURE_SWIPE_DOWN = 2
+            case GESTURE_SWIPE_LEFT:
+                Toast.makeText(getApplication(), "SWIPE_LEFT", Toast.LENGTH_LONG).show();
+                source.performAction(AccessibilityNodeInfo.ACTION_ACCESSIBILITY_FOCUS);
+                return true;
+            case GESTURE_SWIPE_RIGHT:
+                Toast.makeText(getApplication(), "SWIPE_RIGHT", Toast.LENGTH_LONG).show();
+                source.performAction(AccessibilityNodeInfo.ACTION_ACCESSIBILITY_FOCUS);
+                return true;
+            case GESTURE_SWIPE_UP:
+                Toast.makeText(getApplication(), "SWIPE_UP", Toast.LENGTH_LONG).show();
+                return true;
+            case GESTURE_SWIPE_DOWN:
+                Toast.makeText(getApplication(), "SWIPE_DOWN", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                Toast.makeText(getApplication(), "SWIPE_ETC", Toast.LENGTH_LONG).show();
+                return false;
+        }
+    }
 
     // 이벤트가 발생할때마다 실행되는 부분
     @Override
@@ -54,7 +84,7 @@ public class aAccessibilityService extends AccessibilityService {
         Log.e(TAG, "Catch Event getSource : " + event.getSource());
         Log.e(TAG, "=========================================================================");
         // 발생한 이벤트로부터 Source를 get
-        AccessibilityNodeInfo source = event.getSource();
+        source = event.getSource();
         // 실현 시간 상수로서 접근성 서비스에 대한 이벤트 타입 변수 선언 및 생성
         final int eventType =  event.getEventType();
 
@@ -68,12 +98,16 @@ public class aAccessibilityService extends AccessibilityService {
             return;
         }
 
-        // 제스처 발생 시에는 해당 제스처에 대한 기능을 먼저 수행토록 하고, TTS 기능은 사용되지 않도록 유도
-        if(eventType == AccessibilityEvent.TYPE_GESTURE_DETECTION_START || eventType == AccessibilityEvent.TYPE_GESTURE_DETECTION_END) {
+        if(eventType == AccessibilityEvent.TYPE_VIEW_CLICKED || eventType == AccessibilityEvent.TYPE_VIEW_HOVER_ENTER) {
             // 이벤트를 발생시킨 해당 소스에 대한 Action 실행. 이 때의 Action은 접근성 서비스를 위한 FOCUS
             source.performAction(AccessibilityNodeInfo.ACTION_ACCESSIBILITY_FOCUS);
             // 다시 사용할 수 있도록 해당 인스턴스를 반환
             source.recycle();
+        }
+
+        // 제스처 발생 시에는 해당 제스처에 대한 기능을 먼저 수행토록 하고, TTS 기능은 사용되지 않도록 유도
+        else if(eventType == AccessibilityEvent.TYPE_TOUCH_INTERACTION_START) {
+            Toast.makeText(getApplicationContext(), "1111111111111111111111111111111111111", Toast.LENGTH_LONG).show();
         }
 
         else{
@@ -101,7 +135,7 @@ public class aAccessibilityService extends AccessibilityService {
             // 이벤트의 대상이 된 컴포넌트의 ContentDescription 내용을 String에 저장 및 출력
             if(eventText != null) {
                 eventText = eventText + event.getContentDescription();
-                if(eventText.length() < 20)
+                if(eventText.length() < 30)
                     eventText = eventText + introText;
                 Toast.makeText(getApplication(), eventText, Toast.LENGTH_SHORT).show();
 
@@ -139,18 +173,18 @@ public class aAccessibilityService extends AccessibilityService {
 
         // 뷰 클릭 시, 뷰 포커싱 시, 제스처 인식 시, 검색창 텍스트 변경 시 접근성 서비스 이용을 위한 이벤트 타입 수집
         info.eventTypes = AccessibilityEvent.TYPE_VIEW_CLICKED | AccessibilityEvent.TYPE_VIEW_FOCUSED
-                | AccessibilityEvent.TYPE_GESTURE_DETECTION_START | AccessibilityEvent.TYPE_GESTURE_DETECTION_END
+                | AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_START | AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_END
                 | AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED | AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED
-                | AccessibilityEvent.TYPE_VIEW_SELECTED | AccessibilityEvent.TYPE_VIEW_ACCESSIBILITY_FOCUSED;
+                | AccessibilityEvent.TYPE_VIEW_SELECTED | AccessibilityEvent.TYPE_VIEW_ACCESSIBILITY_FOCUSED | AccessibilityEvent.TYPE_VIEW_HOVER_ENTER;
 
         //info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK; // 전체 이벤트 가져오기
 
-        // 이벤트 발생 시 음성 피드백 제공
+        // 이벤트 발생 시 음성 및 진동 피드백 제공
         info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN | AccessibilityServiceInfo.FEEDBACK_HAPTIC;
         //info.feedbackType = AccessibilityServiceInfo.FEEDBACK_ALL_MASK;
 
         // 제스처 기능 수행을 위한 터치 모드 수행 플래그 설정
-        //info.flags = AccessibilityServiceInfo.FLAG_REQUEST_TOUCH_EXPLORATION_MODE | AccessibilityServiceInfo.FLAG_INCLUDE_NOT_IMPORTANT_VIEWS;
+        info.flags = AccessibilityServiceInfo.FLAG_REQUEST_TOUCH_EXPLORATION_MODE;
         //info.flags = AccessibilityServiceInfo.FLAG_REQUEST_FINGERPRINT_GESTURES;
         info.notificationTimeout = 100; // millisecond
         // 서비스 설정
